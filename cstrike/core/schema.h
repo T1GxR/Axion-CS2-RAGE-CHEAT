a5407d28a7d25596a399478824852a1262ee8f9b
T1GXR <106729571+T1GxR@users.noreply.github.com>
2024-04-24 21:15:22 +0300

    Add files via upload

--- cstrike/core/schema.h
+++ cstrike/core/schema.h
@@ -5,56 +5,35 @@
 
 // used: fnv-1a hash
 #include "../utilities/fnv1a.h"
-#define SCHEMA_ADD_OFFSET(TYPE, NAME, OFFSET) \
-    [[nodiscard]] CS_INLINE std::add_lvalue_reference_t<TYPE> NAME() \
-    { \
-        static const std::uint32_t uOffset = OFFSET; \
-        return *reinterpret_cast<std::add_pointer_t<TYPE>>(reinterpret_cast<std::uint8_t*>(this) + (uOffset)); \
-    }
 
-#define SCHEMA_ADD_POFFSET(TYPE, NAME, OFFSET) \
-    [[nodiscard]] CS_INLINE std::add_pointer_t<TYPE> NAME() \
-    { \
-        const static std::uint32_t uOffset = OFFSET; \
-        return reinterpret_cast<std::add_pointer_t<TYPE>>(reinterpret_cast<std::uint8_t*>(this) + (uOffset)); \
-    }
+#define SCHEMA_ADD_OFFSET(TYPE, NAME, OFFSET)                                                                 \
+	[[nodiscard]] CS_INLINE std::add_lvalue_reference_t<TYPE> NAME()                                          \
+	{                                                                                                         \
+		static const std::uint32_t uOffset = OFFSET;                                                          \
+		return *reinterpret_cast<std::add_pointer_t<TYPE>>(reinterpret_cast<std::uint8_t*>(this) + (uOffset)); \
+	}
 
-#define SCHEMA_ADD_FOREIGN_OFFSET(TYPE, NAME, MODULE, CLASS, FIELD) \
-    [[nodiscard]] CS_INLINE std::add_lvalue_reference_t<TYPE> NAME() \
-    { \
-        const static std::uint32_t uOffset = SCHEMA::GetForeignOffset(MODULE, FNV1A::HashConst(CLASS), FNV1A::HashConst(FIELD)); \
-        return *reinterpret_cast<std::add_pointer_t<TYPE>>(reinterpret_cast<std::uint8_t*>(this) + (uOffset)); \
-    }
+#define SCHEMA_ADD_POFFSET(TYPE, NAME, OFFSET)                                                               \
+	[[nodiscard]] CS_INLINE std::add_pointer_t<TYPE> NAME()                                                  \
+	{                                                                                                        \
+		const static std::uint32_t uOffset = OFFSET;                                                         \
+		return reinterpret_cast<std::add_pointer_t<TYPE>>(reinterpret_cast<std::uint8_t*>(this) + (uOffset)); \
+	}
 
-#define SCHEMA_ADD_FIELD_OFFSET(TYPE, NAME, FIELD, ADDITIONAL) \
-    SCHEMA_ADD_OFFSET(TYPE, NAME, SCHEMA::GetOffset(FNV1A::HashConst(FIELD)) + ADDITIONAL)
+#define SCHEMA_ADD_FIELD_OFFSET(TYPE, NAME, FIELD, ADDITIONAL) SCHEMA_ADD_OFFSET(TYPE, NAME, SCHEMA::GetOffset(FNV1A::HashConst(FIELD)) + ADDITIONAL)
 
-#define SCHEMA_ADD_FIELD(TYPE, NAME, FIELD) \
-    SCHEMA_ADD_FIELD_OFFSET(TYPE, NAME, FIELD, 0U)
-
-#define SCHEMA_ADD_FIELD_OFFSET_FOREIGN(TYPE, NAME, MODULE, CLASS, FIELD, ADDITIONAL) \
-    SCHEMA_ADD_FOREIGN_OFFSET(TYPE, NAME, MODULE, CLASS, FIELD)
-
-#define SCHEMA_ADD_FIELD_FOREIGN(TYPE, NAME, MODULE, CLASS, FIELD) \
-    SCHEMA_ADD_FIELD_OFFSET_FOREIGN(TYPE, NAME, MODULE, CLASS, FIELD, 0U)
-
-
-
-
-
+#define SCHEMA_ADD_FIELD(TYPE, NAME, FIELD) SCHEMA_ADD_FIELD_OFFSET(TYPE, NAME, FIELD, 0U)
 
 #define SCHEMA_ADD_PFIELD_OFFSET(TYPE, NAME, FIELD, ADDITIONAL) SCHEMA_ADD_POFFSET(TYPE, NAME, SCHEMA::GetOffset(FNV1A::HashConst(FIELD)) + ADDITIONAL)
 
 #define SCHEMA_ADD_PFIELD(TYPE, NAME, FIELD) SCHEMA_ADD_PFIELD_OFFSET(TYPE, NAME, FIELD, 0U)
-
-
 
 // @todo: dump enums?
 namespace SCHEMA
 {
 	// store the offset of the field in the class
 	// dump stored data to file
-	bool Setup(const wchar_t* wszFileName);
+	bool Setup(const wchar_t* wszFileName, const char* szModuleName);
 
 	/* @section: get */
 	// get offset of the field in the class
